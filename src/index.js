b55493bde17fe26a43bc43a9bcf07d8279a2dede
nikitaSB01 <118895582+nikitaSB01@users.noreply.github.com>
2024-10-20 12:02:12 +0600

    старт

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './css/style.css';
+
+// eslint-disable-next-line import/extensions
+import './js/app.js';
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('node:path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true, // Очищает папку dist перед каждой сборкой
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'], // Используй MiniCssExtractPlugin.loader
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css', // Имя файла для стилей
+    }),
+  ],
+};
