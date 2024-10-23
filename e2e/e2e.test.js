a21649bd102ef351e5c9ba477a182fb3e6788e71
nikitaSB01 <118895582+nikitaSB01@users.noreply.github.com>
2024-10-23 21:41:16 +0600

    изменил "headless: true"

--- e2e/e2e.test.js
+++ e2e/e2e.test.js
@@ -6,7 +6,7 @@ describe('Popover Tests', () => {
 
   beforeAll(async () => {
     browser = await puppeteer.launch({
-      headless: false,
+      headless: true,
       slowMo: 100,
     });
     page = await browser.newPage();
